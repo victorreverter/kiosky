8d219e41dff4efb5b6791388746cbf084936a405
victorreverter <victorreverter@users.noreply.github.com>
2026-06-05 22:46:24 +0200

    Convert the app in a PWA

---
+++ dev-dist/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) navigator.serviceWorker.register('/kiosky/dev-sw.js?dev-sw', { scope: '/kiosky/', type: 'classic' })
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
     react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      base: '/kiosky/',
+      manifest: false,
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,jpg,json}'],
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
+      },
+      devOptions: {
+        enabled: true,
+      },
+    }),
   ],
   base: '/kiosky/',
 })
